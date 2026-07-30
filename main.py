9523f03acfc444a3f80460968965965dcec41520
Anagha Kalyanaraman <99898759+anaghakalyanaraman@users.noreply.github.com>
2026-07-30 08:41:06 -0700

    Update CORS middleware to include production origin
    Added a new origin to CORS middleware for deployment.

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:5174"
+         "https://fitness-coach-ui-5ms1.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
